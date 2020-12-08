c3a17144b12009fb1c3ecf9e342d8db0c7e8bb91
Higor Matheus O Vieira <hmoliveira83@gmail.com>
2020-12-08 16:18:18 -0300

    rodando os testes

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,21 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const userExists = this.customersRepository.findByEmail(email);
+
+    if (userExists) {
+      throw new AppError('Email user exists');
+    }
+
+    const customer = this.customersRepository.create({ name, email });
+
+    return customer;
   }
 }
 
--- src/modules/orders/infra/typeorm/repositories/OrdersRepository.ts
+++ src/modules/orders/infra/typeorm/repositories/OrdersRepository.ts
@@ -13,10 +13,17 @@ class OrdersRepository implements IOrdersRepository {
 
   public async create({ customer, products }: ICreateOrderDTO): Promise<Order> {
     // TODO
+
+    const order = this.ormRepository.create({ customer });
+
+    return order;
   }
 
   public async findById(id: string): Promise<Order | undefined> {
     // TODO
+    const order = this.ormRepository.findOne(id);
+
+    return order;
   }
 }
 
